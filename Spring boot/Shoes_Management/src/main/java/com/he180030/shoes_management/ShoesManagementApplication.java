b94b51cc75c2255fc5882304053318fbbf9bb19f
Tran Xuan Hoan (K18 HL) <hoantxhe180030@fpt.edu.vn>
2024-11-21 15:44:38 +0700

    add spring boot project

---
+++ Spring boot/Shoes_Management/src/main/java/com/he180030/shoes_management/ShoesManagementApplication.java
@@ -0,0 +1,13 @@
+package com.he180030.shoes_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoesManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoesManagementApplication.class, args);
+	}
+
+}
---
+++ Spring boot/Shoes_Management/src/test/java/com/he180030/shoes_management/ShoesManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.he180030.shoes_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoesManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
